dbdb6fceafec422add967509f4ead866aa60a5a7
Victor <victor.mendele68@gmail.com>
2024-12-15 16:21:01 +0100

    feat: support name captilization

--- mod.ts
+++ mod.ts
@@ -3,15 +3,32 @@
  * 
  * @module
  * 
- * @example ```typescript
+ * @example 
+ * Say hello to someone
+ * ```typescript
  * import { hello } from "jsr:@botflux/package-publishing-test"
  * 
  * console.log(hello("John")) // Hello, John!
  * ```
  * 
+ * @example
+ * The name is capitalized
+ * ```typescript
+ * import { hello } from "jsr:@botflux/package-publishing-test"
+ * 
+ * console.log(hello("john")) // Hello, John!
+ * ```
+ * 
+ * 
  * @param name the person you want to say hell to
  * @returns 
  */
 export function hello(name: string): string {
-    return `Hello, ${name}!`
+    return `Hello, ${capitalized(name)}!`
+}
+
+function capitalized (str: string): string {
+    const [ firstLetter, ...rest ] = str
+
+    return [ firstLetter.toUpperCase(), ...rest ].join("")
 }
