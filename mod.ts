632dfd004172124c404eceb3589e1ae610a072d1
Victor <victor.mendele68@gmail.com>
2024-12-15 16:14:22 +0100

    docs: add comments

--- mod.ts
+++ mod.ts
@@ -1,3 +1,9 @@
+/**
+ * Say hello to someone.
+ * 
+ * @param name the person you want to say hell to
+ * @returns 
+ */
 export function hello(name: string): string {
     return `Hello, ${name}!`
 }
